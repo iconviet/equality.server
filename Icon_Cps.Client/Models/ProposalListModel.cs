f139408c994b952489328f870d2e00cbb5456557
Tom Nuen <tomnuen@iconviet.io>
2020-08-02 22:51:29 +0700

    refactored

--- Icon_Cps.Client/Models/ProposalListModel.cs
+++ Icon_Cps.Client/Models/ProposalListModel.cs
@@ -1,6 +1,38 @@
-﻿namespace Icon_Cps.Client.Models
+﻿using System.Collections.Generic;
+using Icon_Cps.Client.Objects;
+using Icon_Cps.Client.Remote;
+
+namespace Icon_Cps.Client.Models
 {
     public class ProposalListModel : ModelBase
     {
+        public List<Proposal> Proposals { get; set; }
+
+        public IconServiceClient ServiceClient { get; set; }
+
+        public ProposalListModel()
+        {
+            Proposals = new List<Proposal>
+            {
+                new Proposal
+                {
+                    Title = "Proposal 1",
+                    IcxRequested = 100000,
+                    UsdRequested = 40000
+                },
+                new Proposal
+                {
+                    Title = "Proposal 2",
+                    IcxRequested = 100000,
+                    UsdRequested = 40000
+                },
+                new Proposal
+                {
+                    Title = "Proposal 3",
+                    IcxRequested = 100000,
+                    UsdRequested = 40000
+                }
+            };
+        }
     }
 }
